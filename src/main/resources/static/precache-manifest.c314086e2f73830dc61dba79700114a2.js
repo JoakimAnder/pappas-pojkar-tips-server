fb0d106a7e516942521467abd3743316b4398632
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-08-21 13:45:48 +0200

    Added the basic website
    with login/registration/profile

---
+++ src/main/resources/static/precache-manifest.c314086e2f73830dc61dba79700114a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72323b9dbd69925a9c7e7810be457db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99590e9abf9529611b42",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "387bdca84dd9369003be",
+    "url": "/static/js/2.5e8037cd.chunk.js"
+  },
+  {
+    "revision": "99590e9abf9529611b42",
+    "url": "/static/js/main.22d1c17a.chunk.js"
+  },
+  {
+    "revision": "f250fa3cfef63180ac9e",
+    "url": "/static/js/runtime~main.776da4c5.js"
+  }
+]);
